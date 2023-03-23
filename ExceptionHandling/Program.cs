8f2a61b80a8f6acd5daacb4c587254a0dea667fc
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-03-23 12:11:14 +0530

    ExecptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 0;
+            int[] d = {a, b};
+            try
+            {
+                //int c = a / b;
+                Console.WriteLine(d[5]);
+            }
+            catch (DivideByZeroException dz) 
+            {
+                Console.WriteLine(dz.Message);
+                Console.WriteLine("Do not try to divide by 0");
+            }
+            catch(IndexOutOfRangeException ie)
+            {
+                Console.WriteLine(ie.Message);
+                Console.WriteLine("You are trying to access a index which is not present");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception - "+e.Message);
+                Console.WriteLine("Catching Exceptions since some time");
+            }
+            finally 
+            { 
+                Console.WriteLine("Finally finished"); 
+            }
+            
+        }
+    }
+}
