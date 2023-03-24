23d32894ff95b72ab0a17537fc451b3f7fc4bb26
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-03-24 10:24:05 +0530

    Re-Organization of files

---
+++ Day 4/Assignment1-2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment1_2
+{
+    internal class Program
+    {
+        public Dictionary<string, int> Chocolates = new Dictionary<string, int>();
+        int TotalChocolates;
+        public ChocolateDispener()
+        {
+            this.Chocolates["Red"] = 6;
+            this.Chocolates["Blue"] = 10;
+            this.Chocolates["Green"] = 20;
+        }
+        public void addChocolates(string color, int count)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
