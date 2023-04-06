44e80db7b30e010a73a317701ebef7d7c477d9f9
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-04-06 17:59:44 +0530

    Initial commit for sql connection through c#

---
+++ SQLConnection/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+namespace SQLConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=HARSHITHP-PC;Initial Catalog=PracticeDatabase;Integrated Security=True;Encrypt=False;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "select * from technologies";
+            SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine($"ID     -    Tech");
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetInt32(1)}    -   {reader.GetString(0)}");
+            }
+            connection.Close();
+        }
+    }
+}
